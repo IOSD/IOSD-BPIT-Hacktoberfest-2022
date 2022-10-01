0cb76b1e9dde3e1526755ecf447aefaed24042c9
Vaibhav <ervaibhavkumar@outlook.com>
2022-10-01 10:42:45 +0530

    Added example logic to show the function usage
    Added the test logic which can give use the clarity on how the function shall be used

--- DSA/Python/Binary_Search.py
+++ DSA/Python/Binary_Search.py
@@ -23,3 +23,15 @@ def binary_search(arr, x):
  
     # If we reach here, then the element was not present
     return -1
+    
+# Test array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+ 
+# Function call
+result = binary_search(arr, x)
+ 
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
