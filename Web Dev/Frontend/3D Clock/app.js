eb11847c08a48de69aef0dc5dd10e7461a4e68c2
rishi2025 <99127485+rishi2025@users.noreply.github.com>
2022-10-04 10:45:14 +0530

    Basic Clock (HTML CSS Javascript)
    This is a basic Clock.
    One can modify and use this template in their projects also.
    Styling Modifications are also welcome

---
+++ Web Dev/Frontend/3D Clock/app.js
@@ -0,0 +1,26 @@
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var median = "AM";
+
+    if (h==0) h=12;
+
+    if (h>12) {
+        h-=12;
+        median = "PM";
+    }
+
+    h = (h<10)?"0"+h:h;
+    m = (m<10)?"0"+m:m;
+    s = (s<10)?"0"+s:s;
+
+    var time = h + ":" + m + ":" + s + " " + median;
+    document.getElementById("Clock").innerText = time;
+    document.getElementById("Clock").textContent = time;
+
+    setTimeout ( showTime , 1000 );
+}
+
+showTime();
